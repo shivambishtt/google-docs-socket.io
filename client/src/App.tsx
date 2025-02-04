dc413e825ed13b69d1ebe2e9dc0974a13ef34367
Shivam Bisht <rajshivam2745@gmail.com>
2025-02-04 20:03:25 +0530

    final feature add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { Editor } from "@tinymce/tinymce-react";
+import tinymce from "tinymce";
 
 const socket: Socket = io("http://localhost:8080");
 const apiKey = import.meta.env.VITE_EDITOR_API_KEY
@@ -8,34 +9,75 @@ const apiKey = import.meta.env.VITE_EDITOR_API_KEY
 function App() {
   const [userTyping, setUserTyping] = useState<string>() // what user is actually typing
   const [typingUsers, setTypingUsers] = useState<Record<string, string>>({})
+  const [cursorPosition, setCursorPosition] = useState<Record<string, number>>({})
 
   const handleEditorChange = (content: string) => {
     setUserTyping(content)
   }
 
-  useEffect(() => {
-    if (socket) {
-      socket.emit("userTyping", userTyping) // client typing data sent to server
+  const handleSelectionChange = () => {
+    const editor = tinymce.activeEditor;
+    if (editor) {
+      const selection = editor.selection;
+      const range = selection.getRng(); // Get current selection range
+      const position = range.startOffset; // Get cursor position
 
-      socket.on("updateTypingUsers", (typingData) => {  //receiving the data from the server of updated type
-        setTypingUsers(typingData)
+      socket.emit("cursorcoordinates", {
+        cursorid: socket.id,
+        position
       })
     }
+  };
 
-    return () => {
-      socket.off("updateTypingUsers")
+  useEffect(() => {
+    if (socket) {
+      socket.emit("userTyping", userTyping); // client typing data sent to server
+
+      socket.on("updatedUserTyping", (typingData) => {
+        setTypingUsers(typingData); // Update the state with the typing data
+      });
+
+      socket.on("updatedcursordetails", (cursorData) => {
+        setCursorPosition((prev) => ({
+          ...prev,
+          [cursorData.cursorid]: cursorData.position,
+        }));
+      });
+
+
+      return () => {
+        socket.off("updatedUserTyping");
+        socket.off("updatedcursordetails");
+      };
     }
-  }, [userTyping])
+  }, [userTyping]);
 
   return (
     <div>
       < Editor
         apiKey={apiKey}
         onEditorChange={handleEditorChange}
+        onInit={(event, editor) => {
+          editor.on("SelectionChange", handleSelectionChange)
+        }}
         init={{
-          plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
-          toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+         
         }} />
+
+      {Object.entries(typingUsers).map(([socketId, text]) => (
+        <li key={socketId}>
+          <b>User {socketId}:</b> {text}
+        </li>
+      ))}
+
+      <h3>Cursor Positions:</h3>
+      <ul>
+        {Object.entries(cursorPosition).map(([socketId, position]) => (
+          <li key={socketId}>
+            <b>User {socketId}:</b> Cursor at {position}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
